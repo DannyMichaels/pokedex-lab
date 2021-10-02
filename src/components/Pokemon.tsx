0d0367d8aceb5eff39cf0a25d8ac97ae400b333f
Daniel <itzdanielmichael@gmail.com>
2021-10-02 00:55:37 -0400

    update types/Pokemon.ts, Pokemon.tsx

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -7,5 +7,20 @@ type PokemonProps = {
 export default function Pokemon({ pokeData }: PokemonProps) {
   const { name, sprites, weight, height } = pokeData;
 
-  return <>Pokemon</>;
+  return (
+    <div>
+      <ul>
+        <h1 className="pokeName">Name: {name}</h1>
+        <li>Height: {height}</li>
+        <li> Weight: {weight}</li>
+        <img
+          className="pokeImg"
+          src={sprites?.front_shiny ?? ''}
+          width="250"
+          height="250"
+          alt={name}
+        />
+      </ul>
+    </div>
+  );
 }
--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -1,7 +1,18 @@
+export type TSprites = {
+  back_default: string;
+  back_female: string;
+  back_shiny: string;
+  back_shiny_female: string;
+  front_default: string;
+  front_female: string;
+  front_shiny: string;
+  front_shiny_female: string;
+};
+
 export interface IPokemon {
   name: string;
   abilities: [];
-  sprites: object;
+  sprites: TSprites;
   species: object;
   stats: [];
   height: number;
